93b36ec5a3029d164ff1b732541bfaa9c44514ec
unknown <aakritin7@gmail.com>
2021-03-18 23:33:20 +0545

    initial commit

---
+++ app/src/main/java/com/example/thelayouteditor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.thelayouteditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.the_layout_editor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
